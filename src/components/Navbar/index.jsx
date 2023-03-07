e1855cb67a64f079a9795ee1da6198d5bafc14b6
Rodrigo Leiva <rleiva.079@gmail.com>
2023-03-07 13:04:59 -0300

    agregamos navbar y componentes

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+        <p>Home</p>
+        <p>Products</p>
+        <p>Contact</p>
+        <p>Our Clients</p>
+        <p>About Us</p>
+    </nav>
+  );
+};
+
+export default Navbar
